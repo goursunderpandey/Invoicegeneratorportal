48cbcb3701a36986533f71de1b2115249305e5cd
Goursunder Pandey <sunderpandey53@gmail.com>
2025-09-28 21:32:53 +0530

    done

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
 const config = {
-    Backendurl : "http://localhost:8080/V1",
-    //Backendurl :  'https://invoice-generatorapi-xacm.vercel.app/V1'
-    Imageurl : "http://localhost:8080/",
-    //Imageurl :  'https://invoice-generatorapi-xacm.vercel.app/'
+  //  Backendurl : "http://localhost:8080/V1",
+    Backendurl :  'https://invoice-generatorapi-xacm.vercel.app/V1',
+   // Imageurl : "http://localhost:8080/",
+    Imageurl :  'https://invoice-generatorapi-xacm.vercel.app/'
 }
 
 export default config;
